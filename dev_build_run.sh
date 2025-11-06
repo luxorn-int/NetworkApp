2b90a7f796233318658cad1a7b83de752e65aee1
Luxorn-PC <Luxorn_int@outlook.com>
2025-11-07 02:20:44 +0300

    Move executable files in bin dir
    + edit dev build script

--- dev_build_run.sh
+++ dev_build_run.sh
@@ -2,11 +2,11 @@ echo -e '\n'----- BUILDING PROJECT -----
 
 cmake -DCMAKE_BUILD_TYPE=Debug -B "build/Debug"
 
-cmake --build build/Debug
+cmake --build build/Debug -j $1
 
 echo -e '\n'----- BUILDING ENDED ----- '\n'
 
 echo -e '\n'----- RUNNING ----- 
 
 
-./build/Debug/Tests/Debug/network_tests
+./build/Debug/bin/network_tests
